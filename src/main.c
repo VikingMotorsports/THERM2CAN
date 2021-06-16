ebc34599ec91a5e023f037d2ee1d63ef48c8ab53
pdxsam2 <samp2@pdx.edu>
2021-06-16 04:13:48 -0700

    updated for segment 6

--- src/main.c
+++ src/main.c
@@ -29,11 +29,9 @@ int main(void)
 	ADC_init();
 	CAN_init();
 
-	blink(GPIOC, GPIO_PIN_13, 5);
-
 	uint32_t mb;
 	uint8_t data[8];
-	data[0] = 1; //TODO: update module_number for each module
+	data[0] = 5; //TODO: update module_number for each module
 	while(1)
 	{
 		uint8_t max= 0, min= 0;
@@ -75,9 +73,9 @@ int main(void)
 		data[2] = max;
 		data[3] = (uint8_t) (sum/N_THERMISTORS);
 		data[4] += 0x24; //- errors); //TODO: UPDATE THESE FOR EACH BOARD
-		data[5] = 0x24;
-		data[6] = 0x47;
-		data[7]= 0x42;
+		data[5] = 0xB4;
+		data[6] = 0xD7;
+		data[7]= 0x46;
 		for(int i= 0; i < 7; ++i)
 		{
 			data[7]+= data[i];
